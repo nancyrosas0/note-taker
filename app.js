8c64d45b603ae6ce62bf6f904761020cccd53d38
Nancy <nancyrosas0@gmail.com>
2021-05-04 18:55:21 -0500

    updated package json

--- app.js
+++ app.js
@@ -24,31 +24,31 @@ app.get("/api/notes", (req, res) => {
     return res.json(notes);
 });
 
-app.post("/api/notes", (req, res) => {
-    var newNote = req.body
-    newNote.id = uuid()
-    notes.push(newNote);
-    fs.writeFile 
-    return res.json(notes)
+// app.post("/api/notes", (req, res) => {
+//     var newNote = req.body
+//     newNote.id = uuid()
+//     notes.push(newNote);
+//     fs.writeFile 
+//     return res.json(notes)
 
-    // TO DO : push the new note into the noes array
-    // updates notes array, rewrite the db.json file
-    // send the response of the updated notes array
+//     // TO DO : push the new note into the noes array
+//     // updates notes array, rewrite the db.json file
+//     // send the response of the updated notes array
     
-});
+// });
 
-app.delete("/api/notes/:id", (req, res) => {
-    var id = req.params.id
-    notes = notes.filter !id
-    fs.writeFile notes
-    return res.json(notes);
-    // get the id from the params
-    // filter through the notes array for the object that DOES NOT contain the id that we're searching for 
-    // a filter creates a new array with what WAS NOT deleted
-    // rewrite the db.json file again
-    // send the response of the filtered notes array
+// app.delete("/api/notes/:id", (req, res) => {
+//     var id = req.params.id
+//     notes = notes.filter !id
+//     fs.writeFile notes
+//     return res.json(notes);
+//     // get the id from the params
+//     // filter through the notes array for the object that DOES NOT contain the id that we're searching for 
+//     // a filter creates a new array with what WAS NOT deleted
+//     // rewrite the db.json file again
+//     // send the response of the filtered notes array
     
-});
+// });
 
   app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
